ea4391880f328fa691383171bdd5b73f1fffa530
BashkimJ <bashkim.jance@mail.polimi.it>
2025-06-16 17:45:26 +0200

    Check on the cars position added, so it can only move on the road!!

--- src/main.cpp
+++ src/main.cpp
@@ -33,9 +33,10 @@ struct Vertex {
 };
 
 class VehicleSimulator : public BaseProject {
-    protected:
+protected:
     float aspectRatio;
-    const glm::mat4 lightView = glm::rotate(glm::mat4(1), glm::radians(-30.0f), glm::vec3(0.0f,1.0f,0.0f)) * glm::rotate(glm::mat4(1), glm::radians(-45.0f), glm::vec3(1.0f,0.0f,0.0f));
+    const glm::mat4 lightView = glm::rotate(glm::mat4(1), glm::radians(-30.0f), glm::vec3(0.0f, 1.0f, 0.0f)) *
+                                glm::rotate(glm::mat4(1), glm::radians(-45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     const glm::vec3 lightDir = glm::vec3(lightView * glm::vec4(0.0f, 0.0f, 1.0f, 1.0f));
     glm::vec3 cameraPos;
     glm::vec3 camTarget;
@@ -45,7 +46,7 @@ class VehicleSimulator : public BaseProject {
     glm::mat4 Prj = glm::mat4(1.0f);
     glm::mat4 View = glm::mat4(1.0f);
     glm::vec3 carPos = glm::vec3(0.0f);
-    glm::vec3 offset = glm::vec3(0.0f,3.0f,2.0f);
+    glm::vec3 offset = glm::vec3(0.0f, 3.0f, 2.0f);
     glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
     float carYaw = 0.0f;
     float cameraYaw = 0.0f;
@@ -53,6 +54,9 @@ class VehicleSimulator : public BaseProject {
     float move_speed = 2.0f;
     bool rotate = false;
     bool camRot = false;
+    bool crash_detected = false;
+    int num_crashes = 0;
+
     //Here we will list all the object needed for our project
 
     DescriptorSetLayout DSLgubo, DSLmesh;
@@ -62,7 +66,7 @@ class VehicleSimulator : public BaseProject {
     Pipeline Pmesh;
 
     Scene SC;
-    std::vector<VertexDescriptorRef>  VDRs;
+    std::vector<VertexDescriptorRef> VDRs;
     std::vector<TechniqueRef> PRs;
 
 
@@ -72,11 +76,11 @@ class VehicleSimulator : public BaseProject {
         windowTitle = "VehicleSimulator";
         windowResizable = GLFW_TRUE;
         initialBackgroundColor = {0.1f, 0.1f, 0.2f, 1.0f};
-        aspectRatio = 4/3;
+        aspectRatio = 4 / 3;
     }
 
     void onWindowResize(int w, int h) {
-        aspectRatio = (float)w / (float)h;
+        aspectRatio = (float) w / (float) h;
         RP.width = w;
         RP.height = h;
     }
@@ -84,34 +88,46 @@ class VehicleSimulator : public BaseProject {
     //Here we initialize the descriptor set layouts and our models
     void localInit() {
         DSLgubo.init(this, {
-                    // this array contains the binding:
-                    // first  element : the binding number
-                    // second element : the type of element (buffer or texture)
-                    // third  element : the pipeline stage where it will be used
-                    {0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS, sizeof(GlobalUniformBufferObject), 1}
-                  });
+                         // this array contains the binding:
+                         // first  element : the binding number
+                         // second element : the type of element (buffer or texture)
+                         // third  element : the pipeline stage where it will be used
+                         {
+                             0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS,
+                             sizeof(GlobalUniformBufferObject), 1
+                         }
+                     });
         DSLmesh.init(this, {
-            // this array contains the binding:
-            // first  element : the binding number
-            // second element : the type of element (buffer or texture)
-            // third  element : the pipeline stage where it will be used
-            {0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS, sizeof(UniformBufferObject), 1},
-            {1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT, 0, 1}
-        });
+                         // this array contains the binding:
+                         // first  element : the binding number
+                         // second element : the type of element (buffer or texture)
+                         // third  element : the pipeline stage where it will be used
+                         {
+                             0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS,
+                             sizeof(UniformBufferObject), 1
+                         },
+                         {1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT, 0, 1}
+                     });
 
         Vmesh.init(this, {
-                  {0, sizeof(Vertex), VK_VERTEX_INPUT_RATE_VERTEX}
-                }, {
-                  {0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, pos),
-                         sizeof(glm::vec3), POSITION},
-                  {0, 1, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, norm),
-                         sizeof(glm::vec3), NORMAL},
-                  {0, 2, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, UV),
-                         sizeof(glm::vec2), UV}
-                });
+                       {0, sizeof(Vertex), VK_VERTEX_INPUT_RATE_VERTEX}
+                   }, {
+                       {
+                           0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, pos),
+                           sizeof(glm::vec3), POSITION
+                       },
+                       {
+                           0, 1, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, norm),
+                           sizeof(glm::vec3), NORMAL
+                       },
+                       {
+                           0, 2, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, UV),
+                           sizeof(glm::vec2), UV
+                       }
+                   });
 
         VDRs.resize(1);
-        VDRs[0].init("VMesh",   &Vmesh);
+        VDRs[0].init("VMesh", &Vmesh);
 
         RP.init(this);
         RP.properties[0].clearValue = {0.6f, 0.8f, 1.0f, 1.0f};
@@ -125,25 +141,27 @@ class VehicleSimulator : public BaseProject {
 
         PRs.resize(1);
         PRs[0].init("Simple", {
-                             {&Pmesh, {//Pipeline and DSL for the first pass
-                                 /*DSLglobal*/{},
-                                 /*DSLlocalChar*/{
-                                        /*t0*/{true,  0, {}}// index 0 of the "texture" field in the json file
-                                     }
-                                    }}
-                              }, /*TotalNtextures*/1, &Vmesh);
+                        {
+                            &Pmesh, {
+                                //Pipeline and DSL for the first pass
+                                /*DSLglobal*/{},
+                                /*DSLlocalChar*/{
+                                    /*t0*/{true, 0, {}} // index 0 of the "texture" field in the json file
+                                }
+                            }
+                        }
+                    }, /*TotalNtextures*/1, &Vmesh);
         DPSZs.uniformBlocksInPool = 3;
         DPSZs.texturesInPool = 4;
         DPSZs.setsInPool = 3;
 
         std::cout << "\nLoading the scene\n\n";
-        if(SC.init(this, /*Npasses*/1, VDRs, PRs, "assets/models/scene.json") != 0) {
+        if (SC.init(this, /*Npasses*/1, VDRs, PRs, "assets/models/scene.json") != 0) {
             std::cout << "ERROR LOADING THE SCENE\n";
             exit(0);
         }
 
         submitCommandBuffer("main", 0, populateCommandBufferAccess, this);
-
     }
 
     // Here you create your pipelines and Descriptor Sets!
@@ -152,8 +170,6 @@ class VehicleSimulator : public BaseProject {
         Pmesh.create(&RP);
 
         SC.pipelinesAndDescriptorSetsInit();
-
-
     }
 
     //Here the cleanup of the pipelines and descriptor sets
@@ -176,7 +192,7 @@ class VehicleSimulator : public BaseProject {
     }
 
     static void populateCommandBufferAccess(VkCommandBuffer commandBuffer, int currentImage, void *Params) {
-        VehicleSimulator *T = (VehicleSimulator *)Params;
+        VehicleSimulator *T = (VehicleSimulator *) Params;
         T->populateCommandBuffer(commandBuffer, currentImage);
     }
 
@@ -193,9 +209,8 @@ class VehicleSimulator : public BaseProject {
 
     //APP logic
     void updateUniformBuffer(uint32_t currentImage) {
-
         gameLogic(window);
-        glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), cameraYaw, glm::vec3(0,0,1));
+        glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), cameraYaw, glm::vec3(0, 0, 1));
         glm::vec4 rotatedOffset = rotation * glm::vec4(offset, 1.0f);
         cameraPos = glm::vec3(rotatedOffset) + carPos;
 
@@ -203,7 +218,7 @@ class VehicleSimulator : public BaseProject {
         up = glm::normalize(glm::vec3(rotatedUp));
 
         Prj = glm::perspective(FOVy, aspectRatio, nearPlane, farPlane);
-        View =  glm::lookAt(cameraPos, carPos, up);
+        View = glm::lookAt(cameraPos, carPos, up);
 
 
         GlobalUniformBufferObject gubo{};
@@ -216,23 +231,23 @@ class VehicleSimulator : public BaseProject {
 
 
         int instanceId;
-        SC.TI[0].I[0].Wm[3] = glm::vec4(carPos,1.0f);
-        if (rotate==true) {
+        SC.TI[0].I[0].Wm[3] = glm::vec4(carPos, 1.0f);
+        if (rotate == true) {
             SC.TI[0].I[0].Wm = glm::rotate(SC.TI[0].I[0].Wm, objectYaw, glm::vec3(0.0f, 1.0f, 0.0f));
             rotate = false;
         }
-        for(instanceId = 0; instanceId < SC.TI[0].InstanceCount; instanceId++) {
-            ubo.mMat   = SC.TI[0].I[instanceId].Wm;
+        for (instanceId = 0; instanceId < SC.TI[0].InstanceCount; instanceId++) {
+            ubo.mMat = SC.TI[0].I[instanceId].Wm;
             ubo.mvpMat = Prj * View * ubo.mMat;
-            ubo.nMat   = glm::inverse(glm::transpose(ubo.mMat));
-            ubo.amb = 0.3f; ubo.gamma = 180.0f; ubo.sColor = glm::vec3(0.8f);
+            ubo.nMat = glm::inverse(glm::transpose(ubo.mMat));
+            ubo.amb = 0.3f;
+            ubo.gamma = 180.0f;
+            ubo.sColor = glm::vec3(0.8f);
             SC.TI[0].I[instanceId].DS[0][0]->map(currentImage, &gubo, 0); // Set 0
-            SC.TI[0].I[instanceId].DS[0][1]->map(currentImage, &ubo, 0);// Set 1
-
+            SC.TI[0].I[instanceId].DS[0][1]->map(currentImage, &ubo, 0); // Set 1
         }
-
-
     }
+
     void gameLogic(GLFWwindow *window) {
         static auto startTime = std::chrono::high_resolution_clock::now();
         static auto lastTime = startTime;
@@ -242,59 +257,126 @@ class VehicleSimulator : public BaseProject {
         lastTime = currentTime;
         const float rotate_speed = 1.0f;
 
-        if (glfwGetKey(window, GLFW_KEY_W) ==GLFW_PRESS) {
-            carPos +=move_speed * glm::vec3(glm::rotate(glm::mat4(1.0f), carYaw,
-                                    glm::vec3(0.0f, 0.0f, 1.0f)) * glm::vec4(0,-1,0,1)) * deltaT;
-
-        }
-
-        if (glfwGetKey(window, GLFW_KEY_S) ==GLFW_PRESS) {
-            carPos +=move_speed * glm::vec3(glm::rotate(glm::mat4(1.0f), carYaw,
-                                    glm::vec3(0.0f, 0.0f, 1.0f)) * glm::vec4(0,1,0,1)) * deltaT;
-        }
-        if (glfwGetKey(window, GLFW_KEY_A) ==GLFW_PRESS) {
-                if (carYaw>=-MAX_YAW) {
-                    carYaw-=deltaT*rotate_speed;
-                    objectYaw=-deltaT*rotate_speed;
-                    rotate = true;
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+            if (check_position(carPos + move_speed * glm::vec3(glm::rotate(glm::mat4(1.0f), carYaw,
+                                                                           glm::vec3(0.0f, 0.0f, 1.0f)) * glm::vec4(
+                                                                   0, -1, 0, 1)) * deltaT) == true) {
+                carPos += move_speed * glm::vec3(glm::rotate(glm::mat4(1.0f), carYaw,
+                                                             glm::vec3(0.0f, 0.0f, 1.0f)) * glm::vec4(0, -1, 0, 1)) *
+                        deltaT;
+                crash_detected = false;
+            }
+            else {
+                if (crash_detected== false) {
+                    num_crashes++;
+                    crash_detected = true;
                 }
-
+            }
         }
-        if (glfwGetKey(window, GLFW_KEY_D) ==GLFW_PRESS) {
-            if (carYaw<=MAX_YAW) {
-                carYaw+=deltaT*rotate_speed;
-                objectYaw=deltaT*rotate_speed;
+
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+            if (check_position(carPos - move_speed * glm::vec3(glm::rotate(glm::mat4(1.0f), carYaw,
+                                                                           glm::vec3(0.0f, 0.0f, 1.0f)) * glm::vec4(
+                                                                   0, -1, 0, 1)) * deltaT) == true) {
+                carPos -= move_speed * glm::vec3(glm::rotate(glm::mat4(1.0f), carYaw,
+                                                             glm::vec3(0.0f, 0.0f, 1.0f)) * glm::vec4(0, -1, 0, 1)) *
+                        deltaT;
+                crash_detected = false;
+            }
+            else {
+                if (crash_detected== false) {
+                    num_crashes++;
+                    crash_detected = true;
+                }
+            }
+        }
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+            if (carYaw >= -MAX_YAW) {
+                carYaw -= deltaT * rotate_speed;
+                objectYaw = -deltaT * rotate_speed;
                 rotate = true;
             }
         }
-        if (glfwGetKey(window, GLFW_KEY_E) ==GLFW_PRESS) {
-                cameraYaw+=deltaT*rotate_speed;
-        }
-        if (glfwGetKey(window, GLFW_KEY_Q) ==GLFW_PRESS) {
-            cameraYaw-=deltaT*rotate_speed;
-        }
-
-        if (glfwGetKey(window, GLFW_KEY_F) ==GLFW_PRESS) {
-            if (move_speed<10.0f) {
-                move_speed=move_speed + 1.0f;
-                std::cout << "move_speed = " << move_speed << std::endl;
-            }
-
-        }
-        if (glfwGetKey(window, GLFW_KEY_C) ==GLFW_PRESS) {
-            if (move_speed>1.0f) {
-                move_speed = move_speed-1.0f;
-                std::cout << "move_speed = " << move_speed << std::endl;
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+            if (carYaw <= MAX_YAW) {
+                carYaw += deltaT * rotate_speed;
+                objectYaw = deltaT * rotate_speed;
+                rotate = true;
             }
         }
+        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+            cameraYaw += deltaT * rotate_speed;
+        }
+        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+            cameraYaw -= deltaT * rotate_speed;
+        }
 
-
-
-
-
-
+        if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
+            if (move_speed < 10.0f) {
+                move_speed = move_speed + 1.0f;
+                std::cout << "move_speed = " << move_speed << std::endl;
+            }
+        }
+        if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS) {
+            if (move_speed > 1.0f) {
+                move_speed = move_speed - 1.0f;
+                std::cout << "move_speed = " << move_speed << std::endl;
+            }
+        }
     }
 
+    bool check_position(glm::vec3 pos) {
+        if (pos.x >= -1.5f && pos.x <= 1.5f) {
+            if (pos.y >= -81.5f && pos.y <= 1.5f) {
+                return true;
+            }
+        }
+
+        if (pos.x >= -49.0f && pos.x <= 41.5f) {
+            if (pos.y >= -1.5f && pos.y <= 1.5f) {
+                return true;
+            }
+        }
+        if (pos.x <= 41.5f && pos.x >= 38.5) {
+            if (pos.y <= 1.5f && pos.y >= -81.5f) {
+                return true;
+            }
+        }
+        if (pos.x <= 41.5f && pos.x >= -137.5) {
+            if (pos.y <= -78.5 && pos.y >= -81.5f) {
+                return true;
+            }
+        }
+        if (pos.x>=-49.5 && pos.x<=-46.5) {
+            if (pos.y<=1.5 && pos.y>=-81.5f) {
+                return true;
+            }
+
+        }
+
+        //Here the other part of the city.
+        if (pos.x<=-102.5 && pos.x>=-105.5) {
+            if (pos.y<=9.5 && pos.y>=-81.5f) {
+                return true;
+            }
+        }
+        if (pos.x<=-105.5 && pos.x>=-137.5) {
+            if (pos.y<=9.5 && pos.y>=6.5) {
+                return true;
+            }
+        }
+        if (pos.x<=-134.5 && pos.x>=-137.5) {
+            if (pos.y<=9.5 && pos.y>=-81.5) {
+                return true;
+            }
+        }
+
+
+
+
+
+            return false;
+    }
 };
 
 
@@ -303,12 +385,10 @@ int main() {
 
     try {
         app.run();
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
 }
-
-
